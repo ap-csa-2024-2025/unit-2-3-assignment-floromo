239393be08d0efb535550208f1e95967a5b04da1
floromo <flormmmp@gmail.com>
2024-09-26 10:20:52 -0700

    in progress

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,21 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("apple", 1));
+    System.out.println(delEnd("apple", 3));
+
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String word, int n){
+    String end = word.substring(word.length() - n, word.length());
+    String begin = word.substring(0, n);
+    return begin.concat(end);
+  }
 
+  public static String delEnd(String word, int n){
+    String newWord = word.substring(0, word.length() - n);
+    return newWord;
+  }
+
+  public static int compareLower(String )
 }
